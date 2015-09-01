5c40ce09a5a3d153c791780aa605df795d61ca64
capmany <jorge@capmany.net>
2015-09-01 18:02:38 +0200

    oops, fat fingering

--- bro_pdns.py
+++ bro_pdns.py
@@ -35,7 +35,7 @@ dns_table = Table('dns', metadata,
 
 def hashfile(filepath):
     sha1 = hashlib.sha1()
-    f = open(filepath, 'rb')add
+    f = open(filepath, 'rb')
     try:
         sha1.update(f.read())
     finally:
@@ -95,7 +95,7 @@ class SQLStore:
     def close(self):
         self.conn.close()
 
-    def upsert_record(self,add query, type, answer, ttl, timestamp, count):
+    def upsert_record(self, query, type, answer, ttl, timestamp, count):
         d = dns_table.c
         n = ts(int(float(timestamp)))
         ttl = ttl != "-" and int(float(ttl)) or None
